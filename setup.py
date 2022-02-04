a141fbd5f07cbb92ac5ff04bd45c05272ecdd89c
miidas <miidas@protonmail.com>
2022-02-05 00:17:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyromaji',
-    version='0.0.1',
+    version='0.0.3',
     author='miidas',
     url='https://github.com/miidas/pyromaji',
     description='Romaji to Hiragana conversion library',
